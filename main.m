38aa5bfa51f24bc3f2881b8dae1a59b2779c13bd
ymweiss <92662865+ymweiss@users.noreply.github.com>
2022-12-15 22:54:24 -0800

    removed computing analytic results for M/M/2/3 queueing system

--- main.m
+++ main.m
@@ -46,25 +46,3 @@ while total_of_customers-1 < limit_customers    % checks that the max number of 
 end
 % report
 report;
-%computing analytic results for M/M/2/3
-f = lambda/mu;
-a_0 = 1;
-a_1 = f^1/factorial(1);
-a_2 = f^2/(factorial(2)*2^(2-2));
-a_3 = f^3/(factorial(2)*2^(3-2));
-P_0 = 1/(a_0+a_1+a_2+a_3);
-P_1 = a_1*P_0;
-P_2 = a_2*P_0;
-P_3 = a_3*P_0;
-lambda_D = lambda*(1-P_3);
-E_X = P_1 + 2*P_2 + 3*P_3;
-E_D = E_X/lambda_D;
-E_W = E_D - 1/mu;
-E_Q = lambda_D*E_W;
-
-%display analytic results
-display(['analytic E[X] = ' num2str(E_X)]);
-display(['analytic E[Q] = ' num2str(E_Q)]);
-display(['analytic E[D] = ' num2str(E_D)]);
-display(['analytic E[W] = ' num2str(E_W)]);
-display(['analytic P_B = ' num2str(P_3)]);
